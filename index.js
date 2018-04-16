ce815ad2783f64681f439b613d25e8c07a5471dc
Liana Mancini <liana.mancini@gmail.com>
2018-04-16 12:00:41 -0400

    add spotify functionality

--- index.js
+++ index.js
@@ -2,24 +2,38 @@
 require("dotenv").config();
 var keys = require('./keys.js');
 var Twitter = require('twitter');
-// var spotify = new Spotify(keys.spotify);
-var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
+var twitter = new Twitter(keys.twitter);
 
 
 var command = process.argv[2];
+var title = process.argv.slice(3);
 
-var params = {count: 20};
 if (command === "my-tweets"){
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    twitter.get('statuses/user_timeline', {count: 20}, function(error, tweets, response) {
         if (!error) {
             for (var i = 0; i < tweets.length; i++){
+                console.log("========================================");
                 console.log(tweets[i].created_at, "\n", tweets[i].text);
+                console.log("========================================");
             }
         }
       });
+} else if (command === "spotify-this-song"){
+    // Artist, song name, preview link, album
+    spotify.search({ type: 'track', query: title }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        console.log("========================================");
+        console.log("========================================");
+        console.log("Artist:", data.tracks.items[0].artists[0].name); 
+        console.log("Song:", data.tracks.items[0].name); 
+        console.log("Album:", data.tracks.items[0].album.name); 
+        console.log("Preview:", data.tracks.items[0].preview_url); 
+        console.log("========================================");
+        console.log("========================================");
+    });
 }
