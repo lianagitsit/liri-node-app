963174dcbb4ccc6c7c08c06e67adb15c3d19e90a
Liana Mancini <liana.mancini@gmail.com>
2018-04-16 11:26:25 -0400

    fix invalid key error bug by properly naming the key variable, twitter call complete

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 
 require("dotenv").config();
-var keys = require('./keys.js'); 
+var keys = require('./keys.js');
 var Twitter = require('twitter');
 // var spotify = new Spotify(keys.spotify);
 var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 
 var command = process.argv[2];
+
+var params = {count: 20};
 if (command === "my-tweets"){
-    client.get('statuses/home_timeline', function(error, tweets, response) {
-        console.log("response here");
-        console.log(tweets);
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
-          console.log(tweets);
+            for (var i = 0; i < tweets.length; i++){
+                console.log(tweets[i].created_at, "\n", tweets[i].text);
+            }
         }
       });
 }
